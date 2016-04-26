6e355fbd3739bf562b9dbd7cbf245953739a5825
John Master <john.s.master@gmail.com>
2016-04-26 14:51:51 -0400

    adds parse route

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+var express = require('express');
+var ParseServer = require('parse-server').ParseServer;
+var app = express();
+
+var api = new ParseServer({
+  databaseURI: process.env.MONGODB_URI || 'mongodb://localhost:27017/ps', // Connection string for your MongoDB database
+  appId: 'ga-js-ps',
+  masterKey: 'secretMasterKey', // Keep this key secret!
+  serverURL: 'http://localhost:1337/parse' // Don't forget to change to https if needed
+});
+
+// Serve the Parse API on the /parse URL prefix
+app.use('/parse', api);
+
+app.listen(process.env.PORT || 4321, function() {
+  console.log('parse-server-example running on port ' + (process.env.PORT || 4321));
+});
